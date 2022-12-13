3a9220734d9987a5f69a0c545f4854999a8609c9
strawhattom <xie.tom7@gmail.com>
2022-12-13 14:22:34 +0100

    refactor: removeing console.log

--- users/users.service.js
+++ users/users.service.js
@@ -7,16 +7,14 @@ const saltRounds = 10;
 
 async function register(username, password) {
     try {
-        if (username === undefined) throw new Error("undefined location");
+        if (username === undefined || password === undefined) throw new Error("undefined parameters");
         const hashedPassword = await bcrypt.hash(password, saltRounds);
         const user = await User.create({
             username,
             password:hashedPassword
         });
-        console.log(`[+] Added user : ${username}:${password}`);
         return user;
     } catch (err) {
-        console.log("[!] No user created");
 		console.error(err);
 		return null;
     }
@@ -26,7 +24,6 @@ async function findAll() {
     try {
         return User.find({}).select('username');
     } catch (err) {
-        console.log("[!] Error");
         console.error(err);
         return false;
     }
@@ -36,7 +33,6 @@ async function getUser(id) {
     try {
         return await User.findOne({id}).select("username role");
     } catch (err) {
-        console.log("[!] Error");
         console.error(err);
         return null;
     }
@@ -46,7 +42,6 @@ async function checkUser(username) {
     try {
         return await User.findOne({username});
     } catch (err) {
-        console.log("[!] Error");
         console.error(err);
         return null;
     }
@@ -58,7 +53,6 @@ async function update(id, property) {
         await User.findOneAndUpdate({id}, property);
         return await getUser(id);
     } catch (err) {
-        console.log("[!] Error");
         console.error(err);
         return null;
     }
@@ -68,7 +62,6 @@ async function deleteUser(id) {
     try {
         return await User.findOneAndDelete({_id:id});
     } catch (err) {
-        console.log("[!] Error");
         console.error(err);
         return null;
     }
@@ -80,16 +73,13 @@ async function verify(username, password) {
         const match = await bcrypt.compare(password, user.password);
         return match;
     } catch (err) {
-        console.log("[!] Error");
         console.error(err);
         return null;
     }
 }
 
-
-
-async function generateJWT(username) {
-    return jwt.sign({sub:username}, process.env.JWT_SECRET);
+async function generateJWT(id) {
+    return jwt.sign({sub:id}, process.env.JWT_SECRET);
 }
 
 module.exports = {
