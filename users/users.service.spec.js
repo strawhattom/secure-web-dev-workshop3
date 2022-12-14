0bd990eae16ad1df627d85a8d81081ebd753255e
strawhattom <xie.tom7@gmail.com>
2022-12-14 18:03:24 +0100

    test & refactor: mock functions and adding tests

--- users/users.service.spec.js
+++ users/users.service.spec.js
@@ -1,59 +1,73 @@
 const service = require('./users.service');
 const User = require('./users.model');
+const bcrypt = require('bcrypt');
 
+
+
+
+// Mock fns
 jest.mock('./users.model');
+const hash = jest.spyOn(bcrypt, 'hash');
+const compare = jest.spyOn(bcrypt, 'compare');
+
+beforeEach(() => {
+    hash.mockClear();
+    compare.mockClear();
+    User.findOne.mockClear();
+})
+
+const _userTemplate = {
+    _id: "6398496a3abda61ed31ad734",                                          // generated mongo ObjectID
+    username: "user",
+    password: "$2b$10$JHsxEB3arf2mCIjL7j2Ltu3zdJ3k7Wqm4eX/MyLvApzoKxAWeX6ue", // bcrypt hash with salt rounds of 10 of 'password'
+    role: "user"                                                              // default role user
+};
 
 describe('User registration', () => {
-    it('Should not register without username', async () => {
-        expect(await service.register(undefined,"password")).toBe(null);
-    });
-    it('Should not register without password', async () => {
-        expect(await service.register("user",undefined)).toBe(null);
+    it('Should not register without neither username nor password', async () => {
+        expect(await service.register(undefined,"password")).toBeNull();
+        expect(hash).toHaveBeenCalledTimes(0);
+        expect(await service.register("user",undefined)).toBeNull();
+        expect(hash).toHaveBeenCalledTimes(0);
     });
     it('Should register the user', async () => {
-        const _doc = {
-            _id: "6398496a3abda61ed31ad734",
-            username: "user",
-            password: "$2a$10$DcRs13wKxeYMj0mqDd3aOusl97cA9Fa9Uw9BCBNixade/W1CgNOgW",
-            role: "user"
-        }
+        const _doc = _userTemplate;
         User.create.mockResolvedValue(_doc);
-        expect(await service.register("user","password")).toBe(_doc);
+        expect(await service.register("user","password")).toStrictEqual(_doc);
+        expect(hash).toHaveBeenCalledTimes(1);
     });
 });
 
 describe('User authentication', () => {
-    // Mock return value
-    const _doc = {
-        _id: "6398496a3abda61ed31ad734",
-        username: "user",
-        password: "$2b$10$JHsxEB3arf2mCIjL7j2Ltu3zdJ3k7Wqm4eX/MyLvApzoKxAWeX6ue", // password from mongodb
-        role: "user"
-    }
-    User.create.mockResolvedValue(_doc);
-    User.findOne.mockReturnValueOnce(_doc).mockReturnValueOnce(null).mockReturnValue(_doc);
+    // Mock
+    const _doc = _userTemplate;
 
     it('Should authenticate with corrects credentials', async () => {
+        User.findOne.mockResolvedValue(_doc);
         const username = "user";
         const password = "password";
-        const verify = await service.verify(username, password);
-        expect(verify).toBe(true);
+        expect(await service.verify(username, password)).toBeTruthy();
+        expect(compare).toHaveBeenCalledTimes(1);                           // one call of bcrypt.compare
     });
+    
     it('Should not authenticate with unknown user', async () => {
+        User.findOne.mockResolvedValue(null);
         const username = "userrr";
         const password = "password";
-        const verify = await service.verify(username, password);
-        expect(verify).toBe(null);
+        expect(await service.verify(username, password)).toBeNull();
+        expect(compare).toHaveBeenCalledTimes(0);                           // no call of bcrypt.compare
     });
+
     it('Should not authenticate with wrong password', async () => {
+        User.findOne.mockResolvedValue(_doc);
         const username = "user";
         const password = "wrong";
-        const verify = await service.verify(username, password);
-        expect(verify).toBe(false);
+        expect(await service.verify(username, password)).toBeFalsy();
+        expect(compare).toHaveBeenCalledTimes(1);                           // one call of bcrypt.compare
     });
     it('Should not authenticate with undefined parameters', async () => {
-        const verify = await service.verify(undefined, undefined);
-        expect(verify).toBe(null);
+        expect(await service.verify(undefined, undefined)).toBeNull();
+        expect(compare).toHaveBeenCalledTimes(0);                           // no call of bcrypt.compare
     });
 })
 
@@ -63,13 +77,54 @@ describe('User research', () => {
         expect(await service.findAll()).toStrictEqual({});
     });
     it('Should return an user', async () => {
-        const _doc = {
-            _id: "6398496a3abda61ed31ad734",
-            username: "user",
-            password: "$2b$10$JHsxEB3arf2mCIjL7j2Ltu3zdJ3k7Wqm4eX/MyLvApzoKxAWeX6ue", // password from mongodb
-            role: "user"
-        }
+        const _doc = _userTemplate;
         User.find.mockResolvedValue(_doc);
-        expect(await service.findOne("6398496a3abda61ed31ad734")).toStrictEqual(_doc)
+        expect(await service.findOne("6398496a3abda61ed31ad734")).toStrictEqual(_doc);
     });
+})
+
+describe('User special functions', () => {
+    describe('Update', () => {
+
+        it('Should hash the new password', async () => {
+            const _doc = _userTemplate;
+            User.findOneAndUpdate.mockResolvedValue(_doc);          // this return the old user (without update)
+            const _propertyToChange = {
+                password:'passwords',
+                role:'admin'
+            };
+            // Mock updated user
+            const _updatedDoc = _doc;
+            _updatedDoc.username = "$2b$10$Ds4xwYfJ.JRlFzR0udNzlOhNCYzAIMAaZZk1501/mLAt3v30iV0wW";
+            User.findOne.mockResolvedValue(_updatedDoc);
+            expect(await service.update(_doc._id, _propertyToChange)).toStrictEqual(_updatedDoc);
+            expect(hash).toHaveBeenCalledTimes(1);                  // one call of bcrypt.hash
+        });
+        it('Should update user\'s informations but not the role', async () => {
+            const _doc = _userTemplate;
+            User.findOneAndUpdate.mockResolvedValue(_doc);          // this return the old user (without update)
+            const _propertyToChange = {
+                username:'new_name',
+                role:'admin'
+            };
+            // Mock updated user
+            const _updatedDoc = _doc;
+            _updatedDoc.username = _propertyToChange.username;
+            User.findOne.mockResolvedValue(_updatedDoc);
+            expect(await service.update(_doc._id, _propertyToChange)).toStrictEqual(_updatedDoc);
+            expect(hash).toHaveBeenCalledTimes(0);                  // no call of bcrypt.hash
+        });
+        it('Should update only known fields such as : username and password', async () => {
+            const _doc = _userTemplate;
+            User.findOneAndUpdate.mockResolvedValue(_doc);          // this return the old user (without update)
+            const _propertyToChange = {
+                user:'new_name',
+                unknown:'field',
+                role:'admin'
+            };
+            User.findOne.mockResolvedValue(_doc);
+            expect(await service.update(_doc._id, _propertyToChange)).toStrictEqual(_doc);
+            expect(hash).toHaveBeenCalledTimes(0);                  // no call of bcrypt.hash
+        });
+    })
 })
